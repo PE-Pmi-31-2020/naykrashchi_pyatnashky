16a6d648fc78dfc45933f09a471594286e1938aa
yaremaStiahar <zapasnyy@gmail.com>
2020-11-07 01:48:20 +0200

    bll

--- app/BLL/BLL/Program.cs
+++ app/BLL/BLL/Program.cs
@@ -4,6 +4,14 @@ using System.Linq;
 
 namespace BLL
 {
+    public enum moves
+    {
+        up,
+        left,
+        right,
+        down
+    }
+
     public class Game
     {
         public int match_id { get; set; }
@@ -33,6 +41,30 @@ namespace BLL
             result = false;
             turns = 0;
             custom_picture = cp;
+        }
+
+        public string hash_layout()
+        {
+            string hash = "";
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    hash += Convert.ToChar(layout[i][j]);
+                }
+            }
+            return hash;
+        }
+
+        public void unhash(string s)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    layout[i][j] = Convert.ToInt32(s[i * size + j]);
+                }
+            }
         }
 
         private List<List<int>> layout_init()
@@ -77,10 +109,202 @@ namespace BLL
                 }
                 if(l[i] == 0)
                 {
-                    inversions += 1 + i / 4;
+                    inversions += 1 + i / size;
                 }
             }
             return (inversions & 1) == 1;
         }
+
+        public bool solved()
+        {
+            int prev = 0;
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if(i != size - 1 || j != size - 1)
+                    {
+                        if (prev >= layout[i][j])
+                            return false;
+                        prev = layout[i][j];
+                    }
+                    else
+                    {
+                        return layout[i][j] == 0;
+                    }
+                }
+            }
+            return true;
+        }
+        
+
+        public void move(moves m)
+        {
+            if(m == moves.up)
+            {
+                if(up())
+                {
+                    turns++;
+                }
+            }
+            if (m == moves.left)
+            {
+                if (left())
+                {
+                    turns++;
+                }
+            }
+            if (m == moves.down)
+            {
+                if (down())
+                {
+                    turns++;
+                }
+            }
+            if (m == moves.right)
+            {
+                if (right())
+                {
+                    turns++;
+                }
+            }
+        }
+
+        private bool down()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if(layout[i][j] == 0)
+                    {
+                        if (i == 0)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            layout[i][j] = layout[i - 1][j];
+                            layout[i - 1][j] = 0;
+                            return true;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+        private bool right()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (layout[i][j] == 0)
+                    {
+                        if (j == 0)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            layout[i][j] = layout[i][j - 1];
+                            layout[i][j - 1] = 0;
+                            return true;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+        private bool up()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (layout[i][j] == 0)
+                    {
+                        if (i == size - 1)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            layout[i][j] = layout[i + 1][j];
+                            layout[i + 1][j] = 0;
+                            return true;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+        private bool left()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    if (layout[i][j] == 0)
+                    {
+                        if (j == size - 1)
+                        {
+                            return false;
+                        }
+                        else
+                        {
+                            layout[i][j] = layout[i][j + 1];
+                            layout[i][j + 1] = 0;
+                            return true;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+        public void print()
+        {
+            Console.Clear();
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write(layout[i][j]);
+                    Console.Write('\t');
+                }
+                Console.WriteLine();
+            }
+        }
     }
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Game g = new Game(1,1,4,"s");
+            while(!g.solved())
+            {
+                g.print();
+                var ch = Console.ReadKey(false).Key;
+                switch (ch)
+                {
+                    case ConsoleKey.UpArrow:
+                        g.move(moves.up);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        g.move(moves.down);
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        g.move(moves.left);
+                        break;
+                    case ConsoleKey.RightArrow:
+                        g.move(moves.right);
+                        break;
+                }
+            }
+            Console.WriteLine("SOLVED!");
+        }
+    }
+
 }
