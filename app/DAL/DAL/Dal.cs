b8ab7c3abfb232a5b26b372c0c08a163a050c392
lady-bug17 <aki.loki17@gmail.com>
2020-12-05 21:24:16 +0200

    login function

--- app/DAL/DAL/Dal.cs
+++ app/DAL/DAL/Dal.cs
@@ -25,17 +25,31 @@ namespace DAL
             db.SaveChanges();
         }
 
-        static List<Match> GetUserMatches(int id)
+        static bool LogIn(string login, string password)
+        {
+            User user = (from users in db.Users
+                         where users.Login == login &&
+                         users.Password == password
+                         select users).FirstOrDefault();
+            if (user != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static List<Match> GetUserMatches(int user_id)
         {
             return (from usermatches in db.Matches
-                    where usermatches.UserId == id
+                    where usermatches.UserId == user_id
                     select usermatches).ToList();
         }
 
-        static Match GetMatch()
+        static Match GetMatch(int match_id)
         {
             return (from usermatches in db.Matches
-                    where usermatches.Result == false
+                    where usermatches.Result == false &&
+                    usermatches.MatchId == match_id
                     select usermatches).FirstOrDefault();
         }
 
