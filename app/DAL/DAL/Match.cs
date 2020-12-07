2d71984bb2a9772088f86dbc4845145411445005
yaremaStiahar <zapasnyy@gmail.com>
2020-12-07 14:24:34 +0200

    f7k in stylecop

--- app/DAL/DAL/Match.cs
+++ app/DAL/DAL/Match.cs
@@ -1,20 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// <copyright file="Match.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 #nullable disable
 
 namespace DAL
 {
+    using System;
+
+    /// <summary>
+    /// class Match for storing in database.
+    /// </summary>
     public partial class Match
     {
+        /// <summary>
+        /// Gets or sets match id primary key.
+        /// </summary>
         public int MatchId { get; set; }
+
+        /// <summary>
+        /// Gets or sets used id foreign key.
+        /// </summary>
         public int? UserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets duration.
+        /// </summary>
         public int? Duration { get; set; }
+
+        /// <summary>
+        /// Gets or sets date and time.
+        /// </summary>
         public DateTime? DateTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets score.
+        /// </summary>
         public int? Score { get; set; }
+
+        /// <summary>
+        /// Gets or sets result.
+        /// </summary>
         public bool? Result { get; set; }
+
+        /// <summary>
+        /// Gets or sets hashed layout.
+        /// </summary>
         public long? Layout { get; set; }
+
+        /// <summary>
+        /// Gets or sets turns.
+        /// </summary>
         public int? Turns { get; set; }
+
+        /// <summary>
+        /// Gets or sets path to picture.
+        /// </summary>
         public string CustomPicture { get; set; }
 
         public virtual User User { get; set; }
--- app/DAL/DAL/User.cs
+++ app/DAL/DAL/User.cs
@@ -1,19 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// <copyright file="User.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 #nullable disable
 
 namespace DAL
 {
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Class user for storing in database.
+    /// </summary>
     public partial class User
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="User"/> class.
+        /// </summary>
         public User()
         {
             Matches = new HashSet<Match>();
         }
 
+        /// <summary>
+        /// Gets or sets user id primary key.
+        /// </summary>
         public int UserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets login.
+        /// </summary>
         public string Login { get; set; }
+
+        /// <summary>
+        /// Gets or sets password.
+        /// </summary>
         public string Password { get; set; }
 
         public virtual ICollection<Match> Matches { get; set; }
